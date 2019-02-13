b505c2d80ade036d7311c5d4ddc91b4d1c4659d3
Mahydit Mahmoud Anber <Mahydit_anber@outlook.com>
2019-02-13 15:18:00 +0200

    Completing the imlementation

--- GlassShopAPI.php
+++ GlassShopAPI.php
@@ -5,10 +5,30 @@ if($request->validate("items"))
 {
     $items = new MySQLHandler("items");
     $response = new ResponseHandler($items);
-    if($request->get__method() === "GET")
-    {
-        $response->handle_get($request->get__resource_id());
+    switch ($request->get__method()) {
+        case 'GET':
+            $response->handle_get($request->get__resource_id());
+            break;
+        case 'POST':
+            echo "Post";
+            $response->handle_post($request->get__parameters());
+            break;
+        case 'PUT':
+            $response->handle_put($request->get__parameters(),$request->get__resource_id());
+            break;
+        case 'DELETE':
+            $response->handle_delete($request->get__resource_id());
+            break;
+        case 'OPTIONS':
+            $response->handle_options($request->get__allowed_methods());
+            break;        
+        default:
+            $response->output_with_error(404,array("error"=>"Method not allowed"));
     }
+    // if($request->get__method() === "GET")
+    // {
+    //     $response->handle_get($request->get__resource_id());
+    // }
     
 }
 
